a18ac683a3b276e83a5e468b3455e635e4b06d76
godfath3r <galatsanosp@gmail.com>
2015-01-31 13:06:49 +0200

    Automatic openwifi if you're not allready connected. For raspberrypi

--- openwifi.sh
+++ openwifi.sh
@@ -1,8 +1,14 @@
-k=`ifconfig | grep wlan0 -A1 | grep inet | awk '{print $2}'`
-echo ${k:0:10}
-if [ "$k" == "192.168.1." ]
+ip=`ifconfig | grep wlan0 -A1 | grep inet | awk '{print $2}'`
+
+# Start wifi only if you're not connected.
+if [ "${ip:0:10}" == "192.168.1." ]
 then
-   echo 'Allready Connection'
+   echo 'Allready Connected'
 else
    echo 'Lets connect'
+   ifconfig wlan0 up
+   wpa_supplicant -BDwext -iwlan0 -c /root/wpa.conf
+   dhcpcd
+   chmod 0777 /dev/tty0
+   systemctl restart openntpd.service
 fi
