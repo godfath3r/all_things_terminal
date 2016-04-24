4192f82b73d4073a5ea8fede68d5671b6ddeab52
godfath3r <galatsanosp@gmail.com>
2016-04-24 19:40:21 +0300

    Don't always send mail.
    Make dyndns request with curl instead of lynx.(not always available)

--- ip_changed.sh
+++ ip_changed.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+# Simple script, it can be run with cron to notify you through notify-send
+# that your internet ip has changed
+
+# set recepient variable in order to get notified by mail
+# you must first configure mailx.
+# recipient='test_mail@host.com'
 
 old=`cat /tmp/ip`
-current=`lynx -dump -hiddenlinks=ignore -nolist http://checkip.dyndns.org:8245/ | grep IP | awk '{print $4}'`
+current=`curl http://checkip.dyndns.org:8245/ | grep "IP" | awk '{gsub(/<.*/, "", $6); print $6}'`
 if [ "$old" != "$current" ]; then
   echo $current > /tmp/ip
   notify-send "Your IP changed to:" $current
-  echo "My new ip now is: " $current | mailx -v -s "IP change" $recipient
+  if [ $recipient ]; then
+    echo "My new ip now is: " $current | mailx -v -s "IP change" $recipient
+  fi
 fi
